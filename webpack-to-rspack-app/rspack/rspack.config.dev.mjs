4c88e11b1c7dd818efbea753218481688a52bb27
hela.ben-khalfallah <helabenkhalfallah@hotmail.fr>
2025-01-29 18:21:02 +0100

    [EVOL]: copying rspack configuration

---
+++ webpack-to-rspack-app/rspack/rspack.config.dev.mjs
@@ -0,0 +1,76 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from '@rspack/cli';
+import { rspack } from '@rspack/core';
+import RefreshPlugin from '@rspack/plugin-react-refresh';
+
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ['chrome >= 87', 'edge >= 88', 'firefox >= 78', 'safari >= 14'];
+
+export default defineConfig({
+	context: __dirname,
+	entry: {
+		main: '../src/index.js',
+	},
+	resolve: {
+		extensions: ['...', '.ts', '.tsx', '.js', '.jsx'],
+	},
+	devServer: {
+		hot: true,
+		port: 9001,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				type: 'asset',
+			},
+			{
+				test: /\.(jsx?|tsx?)$/,
+				use: [
+					{
+						loader: 'builtin:swc-loader',
+						options: {
+							jsc: {
+								parser: {
+									syntax: 'typescript',
+									tsx: true,
+								},
+								transform: {
+									react: {
+										runtime: 'automatic',
+										development: true,
+										refresh: true,
+									},
+								},
+							},
+							env: { targets },
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			template: '../public/index.html',
+		}),
+		new RefreshPlugin(),
+	].filter(Boolean),
+	optimization: {
+		minimizer: [
+			new rspack.SwcJsMinimizerRspackPlugin(),
+			new rspack.LightningCssMinimizerRspackPlugin({
+				minimizerOptions: { targets },
+			}),
+		],
+	},
+	experiments: {
+		css: true,
+	},
+});
